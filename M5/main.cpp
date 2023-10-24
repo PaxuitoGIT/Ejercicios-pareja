6d466da378207300ca5fbbe8a896400e1d664478
PaxuitoGIT <bxu@myuax.com>
2023-10-24 21:48:36 +0200

    factorial fix

--- M5/main.cpp
+++ M5/main.cpp
@@ -41,7 +41,7 @@ inline int suma_inline (int a, int b) {
 
 extern int suma_externa(int a, int b);
 
-int factorial (int n) {
+int factorial(int n) {
     if (n == 0) {
         return 1;
     }
@@ -54,11 +54,12 @@ int main() {
     double resultado_suma_double = suma_double(5.5,3.3);
     double resultado_promedio = promedio(5, 1, 2, 3, 4, 5);
     int base = 2;
+    int n = 10;
     int resultado_potencia1 = potencia(base);
     int resultado_potencia2 = potencia(base, 3);
     int resultado_suma_inline = suma_inline(14,43);
     int resultado_suma_externa = suma_externa(42,64);
-    int resultado_factorial = factorial(5);
+    int resultado_factorial = factorial(n);
 
     mensaje();
     std::cout <<"La suma de 5 y 3 es " << resultado_suma << "\n";
@@ -69,7 +70,7 @@ int main() {
     std::cout << "La potencia de " << base << " a la 3 es " << resultado_potencia2 << "\n";
     std::cout << "La suma inline de 14 y 43 es " << resultado_suma_inline << "\n";
     std::cout << "La suma externa de 42 y 64 es " << resultado_suma_externa << "\n";
-    std::cout << "El factorial de 5 es " << resultado_factorial << "\n";
+    std::cout << "El factorial de " << n << " es " << resultado_factorial << "\n";
 
     return 0;
 }
