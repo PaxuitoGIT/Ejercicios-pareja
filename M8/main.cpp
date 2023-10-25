131c5a0c33a10a1b6e3e7a8515657b2c7f4c3a16
Pizarman13 <dpizagar@myuax.com>
2023-10-25 23:09:27 +0200

    Cambio en M8

--- M8/main.cpp
+++ M8/main.cpp
@@ -8,15 +8,16 @@ void saludar() {
     std::cout << "Bon Giorno" << std::endl;
 }
 
-enum DiaSemana {Lunes, Martes, Miercoles, Jueves, Viernes, Sabado, Domingo};
+enum DiaSemana {Lunes, Martes, Miercoles, Jueves, Viernes,};
 
 int main() {
 
     double radio = 3;
 
     double volumenEsfera = (4/3)*PI*radio*radio*radio;
-
+ 
     std::cout << "el volumen de una esfera de radio " << radio << " es: " << volumenEsfera << std::endl;
+    std::cout << "el valor de PI es: " << PI << std::endl;
 
     saludar();
 
@@ -24,17 +25,21 @@ int main() {
 
     std::cout << "el valor de t es: " << t << std::endl;
 
-    if (t > max) {
-        std::cout << "el valor a superado el maximo valor" << std::endl;
-    } else {
-        std::cout << "el valor no a superado el maximo valor" << std::endl;
+    if (t < max) {
+        std::cout << "el valor de t es menor al maximo valor" << std::endl;
     }
 
     DiaSemana hoy = Martes;
+    DiaSemana ayer = Lunes;
+    DiaSemana manana = Miercoles;
+    DiaSemana pasadoManana = Jueves;
+    DiaSemana fin = Viernes;
 
-    if (hoy == Martes) {
-        std::cout << "Hoy es Martes" << std::endl;
-    }
+    std::cout << "hoy es " << hoy << std::endl;
+    std::cout << "ayer fue " << ayer << std::endl;
+    std::cout << "mañana es " << manana << std::endl;
+    std::cout << "pasadoMañana es " << pasadoManana << std::endl;
+    std::cout << "el finde empieza el  " << fin << std::endl;
 
     return 0;
 }
